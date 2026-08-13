b23964b90c00b2b7c17437daf19d8ca4bb3b52bf
chrispam123 <uopechris@gmail.com>
2026-08-13 06:38:19 +0200

    feature: Seguridad proteger endpoint analize de reuquest excesivas

--- infra/modules/networking/variables.tf
+++ infra/modules/networking/variables.tf
@@ -24,3 +24,15 @@ variable "frontend_bucket_arn" {
   description = "ARN del bucket S3 del frontend para la política OAC"
   type        = string
 }
+
+variable "throttling_burst_limit" {
+  description = "Máximo de peticiones simultáneas permitidas (ráfaga) en API Gateway"
+  type        = number
+  default     = 20
+}
+
+variable "throttling_rate_limit" {
+  description = "Peticiones sostenidas por segundo permitidas en API Gateway"
+  type        = number
+  default     = 10
+}
